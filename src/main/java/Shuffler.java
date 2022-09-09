5b4a4382c31a304e5b70302606b55b5cb778030a
deeshavon <deja.greene@tlgcohort.com>
2022-09-09 14:48:42 -0700

    Implemented shuffle for int[], with basic test.

---
+++ src/main/java/Shuffler.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Shuffler {
+
+  private final Random rng;
+
+
+  public Shuffler(Random rng) {  //<--THIS and line 9 are an inputted CONSTRUCTOR
+    this.rng = rng;
+  }
+
+  public void shuffle(int[] data) {
+    for (int i = data.length - 1; i >= 1; i --) {
+      int t = rng.nextInt(i + 1);
+      if (t != i) {
+        // TODO SWAP!
+        int temp = data[t];
+        data[t] = data[i];
+        data[i] = temp;
+      }
+    }
+// declaration + implementation = definition i.e. lines 12 thru 19
+  }
+}
